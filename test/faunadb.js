bc33f4f4e42c87db84d9807c726e7f9076ebe903
COREY ALIX <corey.alix@infor.com>
2021-12-09 21:57:50 -0500

    Adds working faunadb queries

---
+++ test/faunadb.js
@@ -0,0 +1,51 @@
+import faunadb from "faunadb";
+import { FAUNA_SECRET_KEY } from "../app/globals.js";
+const { query, Client } = faunadb;
+const q = query;
+
+const client = new Client({
+  secret: FAUNA_SECRET_KEY,
+  domain: "db.us.fauna.com",
+});
+
+(async () => {
+  let result = await client.query(
+    q.If(
+      q.Exists(q.Collection("Todos")),
+      null,
+      q.CreateCollection({ name: "Todos" })
+    )
+  );
+  console.log(result);
+
+  result = await client.query(
+    q.Create(q.Collection("Todos"), {
+      data: {
+        description: "100CT WHITE WITH WHITE WIRE",
+        code: "100CT WT WHITE",
+        length: 20,
+        color: "white",
+        wire: "white",
+      },
+    })
+  );
+  console.log(result);
+
+  result = await client.query(
+    q.Map(
+      q.Paginate(q.Documents(q.Collection("Todos"))),
+      q.Lambda("ref", q.Get(q.Var("ref")))
+    )
+  );
+  console.log(result);
+
+  const refs = result.data.map((item) => {
+    const { ref, data } = item;
+    console.log(JSON.stringify(data));
+    return ref;
+  });
+  console.log(refs);
+
+  result = await client.query(q.Delete(refs[0]));
+  console.log(result);
+})();
