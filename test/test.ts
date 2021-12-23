c50f67b85d7c0bcbc6c140d507b4bcc455ec3e74
calix <corey.alix@infor.com>
2021-12-23 16:50:53 -0500

    creates a sample index and queries against it

--- test/test.ts
+++ test/test.ts
@@ -1,21 +1,155 @@
-import { FAUNADB_SERVER_SECRET, domain, CONTEXT } from "../app/globals.js";
-import { tables } from "../app/meta/tables.js";
+import { createClient } from "../app/globals.js";
 import faunadb from "faunadb";
 
 const q = faunadb.query;
 
-export function createDatabase() {
-  if (CONTEXT !== "dev") return;
+interface FaunaException {
+  name: string;
+  message: string;
+  description: string;
+  requestResult: any;
+}
 
-  const client = new faunadb.Client({
-    secret: FAUNADB_SERVER_SECRET,
-    domain: domain,
-  });
+function reportError(
+  faunaError: FaunaException
+) {
+  console.log(faunaError);
+}
 
-  const tableNames = Object.keys(tables);
-  tableNames.forEach(async (tableName) => {
-    const response = await client.query(
-      q.Create(q.Ref("classes"), { name: tableName })
-    );
-  });
+async function loadLatestData(args: {
+  tableName;
+}) {
+  const client = createClient();
+  let response = await client.query(
+    q.Paginate(
+      q.Match(
+        q.Index(
+          `${args.tableName}_updates`
+        )
+      )
+    )
+  );
+  debugger;
+  return response;
+}
+
+async function createUpdateStampIndexOnTable(args: {
+  tableName: string;
+}) {
+  const client = createClient();
+  let response = await client.query(
+    q.CreateIndex({
+      name: `${args.tableName}_updates`,
+      source: q.Collection(
+        args.tableName
+      ),
+      unique: false,
+      serialized: true,
+      terms: [],
+      values: [
+        {
+          field: [
+            "data",
+            "updatestamp",
+          ],
+          reverse: true,
+        },
+        { field: ["ref"] },
+      ],
+    })
+  );
+  console.log({ args, response });
+  return response;
+}
+
+async function injectDataWithTimestamp(args: {
+  data: any;
+  tableName: string;
+}) {
+  const client = createClient();
+  const response = await client.query(
+    q.Create(
+      q.Collection(args.tableName),
+      {
+        data: {
+          ...args.data,
+          createstamp: Date.now,
+          updatestamp: Date.now,
+        },
+      }
+    )
+  );
+  return response;
+}
+
+export function init() {
+  const runButton =
+    document.querySelector(
+      "#run"
+    ) as HTMLButtonElement;
+
+  runButton.addEventListener(
+    "click",
+    () => {
+      run();
+    }
+  );
+}
+
+async function run() {
+  const tableName = "Todos";
+
+  try {
+    if (
+      !localStorage.getItem(
+        "test1.createUpdateStampIndexOnTable"
+      )
+    ) {
+      const response =
+        await createUpdateStampIndexOnTable(
+          {
+            tableName,
+          }
+        );
+
+      localStorage.setItem(
+        "test1.createUpdateStampIndexOnTable",
+        JSON.stringify(response)
+      );
+    }
+
+    {
+      const response =
+        await loadLatestData({
+          tableName,
+        });
+      localStorage.setItem(
+        "test1.loadLatestData",
+        JSON.stringify(response)
+      );
+    }
+    if (
+      !localStorage.getItem(
+        "test1.injectDataWithTimestamp"
+      )
+    ) {
+      debugger;
+      const response =
+        await injectDataWithTimestamp({
+          tableName,
+          data: {
+            value: "test1",
+          },
+        });
+
+      debugger;
+
+      localStorage.setItem(
+        "test1.injectDataWithTimestamp",
+        JSON.stringify(response)
+      );
+    }
+  } catch (ex) {
+    reportError(ex as FaunaException);
+  }
 }
