45a5a414d73886998f4d21b8352e0b6021fdd6db
calix <corey.alix@infor.com>
2021-12-27 21:02:01 -0500

    set taxrate if not already set

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,18 @@
 import { setMode } from "./fun/setMode";
+import {
+  getGlobalState,
+  setGlobalState,
+} from "./globals";
 
 export function init() {
   setMode();
+  setInitialState();
+}
+
+function setInitialState() {
+  const taxRate =
+    getGlobalState("TAX_RATE")?.value ||
+    null;
+  if (null == taxRate)
+    setGlobalState("TAX_RATE", 6.0);
 }
